6130c2e23b78da8994c6c4264eef84f887d7dc0b
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2022-09-05 16:53:10 -0300

    arrays de referencias a partir do main method

---
+++ src/br/com/bytebank/bank/inherited/test/TestArrayString.java
@@ -0,0 +1,10 @@
+package br.com.bytebank.bank.inherited.test;
+
+public class TestArrayString {
+    public static void main(String[] args) {
+        System.out.println("Funcionou");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
