0f7cff83e6bb61d9baffe9a7a93d51dee000284d
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2022-09-05 15:59:05 -0300

    arrays de referencias com object e class casting

--- src/br/com/bytebank/bank/inherited/test/TestArraysReferencias.java
+++ src/br/com/bytebank/bank/inherited/test/TestArraysReferencias.java
@@ -1,18 +1,27 @@
 package br.com.bytebank.bank.inherited.test;
 
 import br.com.bytebank.bank.inherited.models.Cliente;
+import br.com.bytebank.bank.inherited.models.Conta;
 import br.com.bytebank.bank.inherited.models.ContaCorrente;
+import br.com.bytebank.bank.inherited.models.ContaPoupacanca;
 
 public class TestArraysReferencias {
     public static void main(String[] args) {
-        ContaCorrente[] contaCorrentes = new ContaCorrente[5];
+        Object[] referencias = new Object[5];
         Cliente cliente = new Cliente();
-        for (int i = 0; i < contaCorrentes.length; i++) {
-            ContaCorrente cc = new ContaCorrente(cliente, i, i);
-            contaCorrentes[i] = cc;
-            contaCorrentes[i].deposita(32.00);
-            System.out.println(contaCorrentes[i].getSaldo());
-            System.out.println(contaCorrentes[i]);
+        for (int i = 0; i < referencias.length; i++) {
+            if (i % 2 == 0) {
+                ContaCorrente cc = new ContaCorrente(cliente, i, i);
+                referencias[i] = cc;
+            } else {
+                ContaPoupacanca cp = new ContaPoupacanca(cliente, i, i);
+                referencias[i] = cp;
+            }
+            ((Conta) referencias[i]).deposita(32.00);
+            System.out.println(((Conta) referencias[i]).getSaldo());
+            System.out.println(referencias[i]);
         }
+        ContaCorrente ref = (ContaCorrente) referencias[2];
+        System.out.println(ref.getAgencia());
     }
 }
