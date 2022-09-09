d65f79eafe58a63189ce278058ea0cd1e1dfbf1a
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2022-09-08 22:09:58 -0300

    ArrayList with generics

--- src/br/com/bytebank/bank/inherited/test/TestArrayList.java
+++ src/br/com/bytebank/bank/inherited/test/TestArrayList.java
@@ -9,25 +9,25 @@ import br.com.bytebank.bank.inherited.models.ContaPoupacanca;
 
 public class TestArrayList {
     public static void main(String[] args) {
-        ArrayList lista = new ArrayList();
+        ArrayList<Conta> lista = new ArrayList<Conta>();
         Cliente cliente = new Cliente("profissao", "nome", "cpf");
-        Conta conta = new ContaCorrente(cliente, 123, 321);
-        lista.add(conta);
+        Conta contaC = new ContaCorrente(cliente, 123, 321);
+        lista.add(contaC);
         Conta contaP = new ContaPoupacanca(cliente, 456, 789);
         Conta contaP1 = new ContaPoupacanca(cliente, 4561, 7891);
         Conta contaP2 = new ContaPoupacanca(cliente, 4562, 7892);
         Conta contaP3 = new ContaPoupacanca(cliente, 4563, 7893);
         lista.add(contaP);
         System.out.println("Tamanho da lista " + lista.size());
-        Conta ref = (Conta) lista.get(0);
+        Conta ref = lista.get(0);
         System.out.println(ref.getNumero());
         lista.remove(0);
         System.out.println("Tamanho da lista " + lista.size());
         lista.add(contaP1);
         lista.add(contaP2);
         lista.add(contaP3);
-        for (Object object : lista) {
-            System.out.println(object);
+        for (Conta conta : lista) {
+            System.out.println(conta);
             System.out.println("------------------------");
         }
 
